2b384ea2a04b8cb95d134f8b3b028650c857b5e5
henderiw <wim.henderickx@gmail.com>
2021-09-09 11:48:08 +0200

    added debug

--- pkg/parser/yang.go
+++ pkg/parser/yang.go
@@ -150,8 +150,17 @@ func (p *Parser) CreateContainerEntry(e *yang.Entry, next, prev *container.Conta
 			entry.Range = append(entry.Range, int(ra.Min.Value))
 			if ra.Max.Value < ra.Min.Value {
 				fmt.Printf("RANGE MIN: %d MAX: %d, TOTAL: %d\n", ra.Min.Value, ra.Max.Value, entry.Range)
+				switch {
+				case strings.Contains(entry.Type, "8"):
+					entry.Range = append(entry.Range, int(255))
+				case strings.Contains(entry.Type, "16"):
+					entry.Range = append(entry.Range, int(65535))
+				default:
+					entry.Range = append(entry.Range, int(4294967295))
+				}
+			} else {
+				entry.Range = append(entry.Range, int(ra.Max.Value))
 			}
-			entry.Range = append(entry.Range, int(ra.Max.Value))
 			//fmt.Printf("RANGE MIN: %d MAX: %d, TOTAL: %d\n", ra.Min.Value, ra.Max.Value, entry.Range)
 		}
 
