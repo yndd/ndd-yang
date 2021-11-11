bcf7ecbf0b58e2a5828cdf4622846505424aad1b
henderiw <wim.henderickx@gmail.com>
2021-11-11 16:43:59 +0100

    added yang pkg

--- pkg/yentry/yentry.go
+++ pkg/yentry/yentry.go
@@ -12,3 +12,19 @@ type Handler interface {
 }
 
 type HandleInitFunc func(parent interface{}) Handler
+
+func (e *Entry) GetName() string {
+	return e.Name
+}
+
+func (e *Entry) GetKey() []string {
+	return e.Key
+}
+
+func (e *Entry) GetParent() Handler {
+	return e.Parent
+}
+
+func (e *Entry) GetChildren() map[string]Handler {
+	return e.Children
+}
