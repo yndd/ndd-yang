5c288d69a43fa8f5c1233721f327714832650e90
henderiw <wim.henderickx@gmail.com>
2021-11-12 07:24:45 +0100

    added logger in yentry

--- pkg/yentry/yentry.go
+++ pkg/yentry/yentry.go
@@ -1,6 +1,9 @@
 package yentry
 
-import "github.com/openconfig/gnmi/proto/gnmi"
+import (
+	"github.com/openconfig/gnmi/proto/gnmi"
+	"github.com/yndd/ndd-runtime/pkg/logging"
+)
 
 type Entry struct {
 	Name     string
@@ -9,7 +12,16 @@ type Entry struct {
 	Children map[string]Handler
 }
 
+type HandlerOption func(Handler)
+
+func WithLogging(log logging.Logger) HandlerOption {
+	return func(o Handler) {
+		o.WithLogging(log)
+	}
+}
+
 type Handler interface {
+	WithLogging(log logging.Logger)
 	GetKeys(p *gnmi.Path) []string
 }
 
