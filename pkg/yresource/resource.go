b335fd5ef13dc381daf9ada14f7def4087833ad4
henderiw <wim.henderickx@gmail.com>
2021-11-18 05:36:46 +0100

    adding yResource

---
+++ pkg/yresource/resource.go
@@ -0,0 +1,45 @@
+/*
+Copyright 2021 Yndd.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package yresource
+
+import (
+	"github.com/openconfig/gnmi/proto/gnmi"
+	"github.com/yndd/ndd-runtime/pkg/logging"
+	"github.com/yndd/ndd-runtime/pkg/resource"
+)
+
+type Resource struct {
+	log      logging.Logger
+	rootPath []*gnmi.Path
+}
+
+func (r *Resource) WithLogging(log logging.Logger) {
+	r.log = log
+}
+
+type Handler interface {
+	WithLogging(log logging.Logger)
+	GetRootPath(mg resource.Managed) ([]*gnmi.Path, error)
+}
+
+type HandlerOption func(Handler)
+
+func WithLogging(log logging.Logger) HandlerOption {
+	return func(o Handler) {
+		o.WithLogging(log)
+	}
+}
