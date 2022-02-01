df534cbfa443cc940b165560157653be66be04b4
henderiw <wim.henderickx@gmail.com>
2022-02-01 05:22:47 +0100

    added getUpdatesFromJSON logic

--- pkg/cache/cache.go
+++ pkg/cache/cache.go
@@ -237,7 +237,15 @@ func (c *Cache) GetNotificationFromUpdate(prefix *gnmi.Path, u *gnmi.Update) (*g
 		return nil, nil
 	case map[string]interface{}:
 		if len(value) == 0 { // this covers an empty map[string]interface{} e.g. routing-policy/policy/action/accept map[string]interface{}
-			updates = append(updates, u)
+			p := c.p.DeepCopyGnmiPath(u.GetPath())
+			if len(p.GetElem()) > 0 {
+				update := &gnmi.Update{
+					Path: &gnmi.Path{Elem: p.GetElem()[:len(p.GetElem())-1]},
+					Val:  &gnmi.TypedValue{Value: &gnmi.TypedValue_JsonVal{JsonVal: []byte(p.GetElem()[len(p.GetElem())].GetName())}},
+				}
+				updates = append(updates, update)
+			}
+
 		}
 		for k, v := range value {
 			k = strings.Split(k, ":")[len(strings.Split(k, ":"))-1]
